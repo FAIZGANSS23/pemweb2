34d2bf95fabe85c0705d02fa6afc23647b0c385e
Faiza Azka <faizaazkawahyuramadhan@saintek.unipdu.ac.id>
2025-04-25 10:47:14 +0700

    editing routing dan controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/halaman', 'Page::index');
+$routes->get('/tos', 'Page::tos');
+$routes->get('/biodata', 'Page::biodata');
