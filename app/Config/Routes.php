816e42f53def4a0e2273322f10c27f1f05830495
Faiza Azka <faizaazkawahyuramadhan@saintek.unipdu.ac.id>
2025-06-22 16:34:43 +0700

    Selesai gabung konflik di routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-<<<<<<< HEAD
+
+// Route dari tugas sebelumnya
 $routes->get('/matkul', 'Aku::matkul');
 $routes->get('/hobi', 'Aku::hobi');
 $routes->get('/proyek', 'Aku::film');
@@ -13,19 +14,19 @@ $routes->get('/beranda', 'Aku::beranda');
 $routes->get('/daftar-game', 'Aku::daftargame');
 $routes->get('/cara-topup', 'Aku::caratopup');
 
-// Routes untuk Books CRUD
+// Route dari tugas baru
 $routes->group('books', function ($routes) {
     $routes->get('/', 'Books::index');
     $routes->get('create', 'Books::create');
-    $routes->post('save', 'Books::save'); // Pastikan ini POST
+    $routes->post('save', 'Books::save');
     $routes->get('edit/(:segment)', 'Books::edit/$1');
     $routes->post('update/(:num)', 'Books::update/$1');
     $routes->delete('(:num)', 'Books::delete/$1');
-    $routes->get('(:segment)', 'Books::detail/$1'); // Letakkan paling akhir
+    $routes->get('(:segment)', 'Books::detail/$1');
 });
-=======
+
+// Route tambahan dari GitHub
 $routes->get('/', 'Home::index');
 $routes->get('/halaman', 'Page::index');
 $routes->get('/tos', 'Page::tos');
 $routes->get('/biodata', 'Page::biodata');
->>>>>>> 34d2bf95fabe85c0705d02fa6afc23647b0c385e
